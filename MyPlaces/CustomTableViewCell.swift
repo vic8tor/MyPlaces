04baba446c7f96f169721deac36cb8833150365a
vic8tor <timongrinni@gmail.com>
2022-05-22 12:22:51 +0300

    Add TableViewCell

--- MyPlaces/CustomTableViewCell.swift
+++ MyPlaces/CustomTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TableViewCell: UITableViewCell {
+class CustomTableViewCell: UITableViewCell {
     
     @IBOutlet weak var customImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
